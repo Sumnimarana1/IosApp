d90d801733142e4e9e622fa021718dd28e716d64
Kancharla,Sravya <s531500@nw42142.nwmissouri.edu>
2018-10-07 21:28:20 -0500

    Back to admin page is working now

---
+++ UserSignin/FormViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FormViewController.swift
+//  UserSignin
+//
+//  Created by Kancharla,Sravya on 10/7/18.
+//  Copyright © 2018 Student. All rights reserved.
+//
+
+import UIKit
+
+class FormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func Save(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
