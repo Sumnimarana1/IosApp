6a2d418a0c3ce0bf65d2a145be68eb99eebaeba8
SuprajaKumbam <supraja.kumbam20@gmail.com>
2018-10-21 11:19:27 -0700

    eventDetailView update

--- UserSignin/HomeViewController.swift
+++ UserSignin/HomeViewController.swift
@@ -21,6 +21,8 @@ class HomeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+
+    
     @IBAction func event1(_ sender: Any) {
         if let events=storyboard?.instantiateViewController(withIdentifier: "EventViewController") as? EventViewController{
             events.evntImage=""
