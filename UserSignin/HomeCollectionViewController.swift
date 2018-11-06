bc1e6e960ec17723fd9a786d90acf1448289b553
Kancharla,Sravya <s531500@ol10019m.nwmissouri.edu>
2018-11-05 18:07:41 -0600

    Rectified error

--- UserSignin/HomeCollectionViewController.swift
+++ UserSignin/HomeCollectionViewController.swift
@@ -9,27 +9,26 @@
 import UIKit
 
 class HomeCollectionViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
- 
+    
     var events:[EventData]=[]
-  
+    
     @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
-        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "appimage5.jpg")!)
         super.viewDidLoad()
         collectionView.delegate=self
         collectionView.dataSource=self
         
-         events.append(EventData(imageName: "", eventTitle: "EventTitle", eventDescription: "EventDescritpion", eventType: "EventType"))
+        events.append(EventData(imageName: "", eventTitle: "EventTitle", eventDescription: "EventDescritpion", eventType: "EventType"))
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     override func viewWillAppear(_ animated: Bool) {
-            collectionView.reloadData()
+        collectionView.reloadData()
     }
     
     
@@ -50,7 +49,7 @@ class HomeCollectionViewController: UIViewController,UICollectionViewDelegate,UI
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         //print(indexPath.row)
-         if let eventController=storyboard?.instantiateViewController(withIdentifier: "EventViewController") as? EventViewController{
+        if let eventController=storyboard?.instantiateViewController(withIdentifier: "EventViewController") as? EventViewController{
             eventController.evntImage=self.events[indexPath.row].imageName
             eventController.evntName=self.events[indexPath.row].eventTitle
             eventController.evntDescription=self.events[indexPath.row].eventDescription
@@ -58,15 +57,15 @@ class HomeCollectionViewController: UIViewController,UICollectionViewDelegate,UI
             self.present(eventController, animated: true, completion: nil)
         }
     }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
